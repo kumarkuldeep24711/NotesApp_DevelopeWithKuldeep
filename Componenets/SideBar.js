11c8f3615ddc5a18601ac66359d2db4c9d2b9f8a
Kuldeep Kumar <103712674+kumarkuldeep24711@users.noreply.github.com>
2022-07-14 17:59:55 +0530

    Add files via upload

---
+++ Componenets/SideBar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useState } from 'react';
+import plusIcon from '../Assest/plus.png';
+import './SideBar.css';
+export default function SideBar(props) {
+    const colors =["#fe9b72", "#fec971 ", "#00d4fe ", "#b693fd","#e4ee91"]
+    const [listOpen,setListOpen]=useState(false);
+  return (
+    <div className = 'sidebar'>
+        <img src={plusIcon} alt="Add"  onClick={()=>setListOpen(!listOpen)}/>
+        <ul className={`sidebar_list ${listOpen?"sidebar_list_active" :" "}`}>
+            {colors.map((item,index)=>(
+                <li
+                key = {index} 
+                className='sidebar_list_item'
+                style ={{backgroundColor :item}}
+                onClick={()=>props.addNote(item)}
+                />
+            ))
+            
+            }
+            
+        </ul>
+    </div>
+  )
+}
